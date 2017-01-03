b7e3be5a46c029bf22f371ba92b1bfd9bd87c074
Yelong Huang <hye_long@hotmail.com>
2017-01-02 21:08:37 -0800

    first commit

---
+++ app/src/main/java/utils/homebutton/OnHomePressedListener.java
@@ -0,0 +1,11 @@
+package utils.homebutton;
+
+/**
+ * Created by yhuang on 1/2/17.
+ */
+
+public interface OnHomePressedListener {
+    public void onHomePressed();
+
+    public void onHomeLongPressed();
+}
